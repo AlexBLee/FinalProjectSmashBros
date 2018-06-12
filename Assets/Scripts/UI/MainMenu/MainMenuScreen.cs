3aa0a136611e3ced48a9b285553ac8e0208d71d5
DESKTOP-8SBE3VE\Alex Lee <alexanderbevislee@gmail.com>
2018-06-11 19:54:52 -0700

    Main menu and the play menu now has functionality with the buttons.

--- Assets/Scripts/UI/MainMenu/MainMenuScreen.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuScreen.cs
@@ -6,6 +6,47 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScreen : MonoBehaviour
 {
+    public Button playButton;
+    public Button trainingButton;
+    public Button optionsButton;
+    public Button quitButton;
 
+    private const string TRAINING_MODE = "Training";
+
+    void Start ()
+    {
+        if (playButton != null)
+        {
+            playButton.onClick.AddListener(MainMenuUIManager.instance.ToPlayScreen);
+        }
+
+        if (trainingButton != null)
+        {
+            trainingButton.onClick.AddListener(ToTraining);
+        }
+
+        if (optionsButton != null) 
+        {
+            optionsButton.onClick.AddListener(MainMenuUIManager.instance.ToOptionsScreen);
+        }
+
+        if (quitButton != null)
+        {
+            optionsButton.onClick.AddListener(Quit);
+        } 
+    }
+
+    private void ToTraining()
+    {
+        SceneManager.LoadScene(TRAINING_MODE);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+
+
+    
 
 }
--- Assets/Scripts/UI/MainMenu/PlayScreen.cs
+++ Assets/Scripts/UI/MainMenu/PlayScreen.cs
@@ -6,6 +6,36 @@ using UnityEngine.SceneManagement;
 
 public class PlayScreen : MonoBehaviour
 {
+    public Button soloModeButton;
+    public Button multiplayerButton;
+
+    private const string CHARACTER_SELECT = "CharacterSelect";
+    private const string MULTIPLAYER = "CharacterSelect";
+    
+
+    void Start ()
+    {
+        if (soloModeButton != null)
+        {
+            soloModeButton.onClick.AddListener(ToCharacterSelect);
+        }
+
+        if (multiplayerButton != null)
+        {
+            multiplayerButton.onClick.AddListener(ToMultiplayer);
+        }
+    }
+
+    private void ToCharacterSelect()
+    {
+        SceneManager.LoadScene(CHARACTER_SELECT);
+    }
+
+    private void ToMultiplayer()
+    {
+        SceneManager.LoadScene(MULTIPLAYER);
+    }
+
 
 
 }

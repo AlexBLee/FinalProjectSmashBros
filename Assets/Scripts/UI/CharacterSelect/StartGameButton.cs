e83157281eff51cecb3aca1806463d9918c5b0ce
DESKTOP-8SBE3VE\Alex Lee <alexanderbevislee@gmail.com>
2018-06-13 13:29:46 -0700

    Made 2nd level

--- Assets/Scripts/UI/CharacterSelect/StartGameButton.cs
+++ Assets/Scripts/UI/CharacterSelect/StartGameButton.cs
@@ -22,7 +22,7 @@ public class StartGameButton : MonoBehaviour
 	{
 		if(GameManager.instance.ready == true)
 		{
-			SceneManager.LoadScene("TestMap");
+			SceneManager.LoadScene("LevelSelect");
 		}
 	}
 
